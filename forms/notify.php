c9fcc2e2186fa330c9dcc77dc91488be77ecefa5
Plug-Ins <110816490+Plug-Ins@users.noreply.github.com>
2022-08-12 15:28:06 +0530

    Update notify.php

--- forms/notify.php
+++ forms/notify.php
@@ -24,14 +24,14 @@
   $contact->subject ="Notify me request";
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'plugins.in@gmail.com',
+    'password' => 'Sanjayplugins',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'Name');
   $contact->add_message( $_POST['email'], 'Email');
